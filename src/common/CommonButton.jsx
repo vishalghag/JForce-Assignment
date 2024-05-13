2001eb53f3770fb2744620769c7bab0c349cf099
vishalghag <vishalghag47@gmail.com>
2024-05-13 17:25:55 +0530

    date picker added

--- src/common/CommonButton.jsx
+++ src/common/CommonButton.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const CommonButton = ({ buttonName, buttonOnClick, btnName }) => {
   return (
-    <div className="flex items-center justify-center mb-4 mr-4">
+    <div className="flex items-center justify-center mb-4 md:mr-4 w-auto ">
       <button
         onClick={buttonOnClick}
         value={btnName}
-        className="block bg-orange-500 text-white py-2 px-8 rounded-md shadow-lg transform transition-transform duration-200 ease-in-out active:scale-95 w-full md:w-auto"
+        className="block bg-orange-500 text-white py-2 px-8 rounded-md shadow-lg transform transition-transform duration-200 ease-in-out active:scale-95 w-full m-2 p-2 md:w-auto"
       >
         {buttonName}
       </button>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import CommonButton from "../common/CommonButton";
+import CommponInput from "../common/CommponInput";
 
 const Todo = () => {
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const addTaskText = useRef(null);
+
+  const showDatePickerFn = () => {
+    console.log("clicked");
+    setShowPicker(!showPicker);
+  };
+
+  const addTodoBtn = () => {
+    setShowPicker(!showPicker);
+    let data = {
+      date: selectedDate,
+      text: addTaskText.current.value,
+    };
+    console.log(data);
+  };
+
+  const showDate = (e) => {
+    setSelectedDate(e.target.value);
+  };
+
   return (
     <div>
       <Header />
+      <div className=" mt-10">
+        <div className=" flex flex-row justify-center items-center">
+          {!showPicker && (
+            <CommonButton
+              buttonName={"Add Todo"}
+              buttonOnClick={showDatePickerFn}
+            />
+          )}
+        </div>
+        {showPicker && (
+          <>
+            <CommponInput
+              inputType={"date"}
+              monitorState={showDate}
+              inputValue={selectedDate}
+            />
+            <CommponInput
+              inputType={"text"}
+              inputPlaceholder={"Add todo text"}
+              inputRef={addTaskText}
+            />
+            <CommonButton buttonName={"Add todo"} buttonOnClick={addTodoBtn} />
+            {selectedDate && <p>Selected Date: {selectedDate}</p>}
+          </>
+        )}
+      </div>
     </div>
   );
 };
